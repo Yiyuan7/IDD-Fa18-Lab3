b59c6f7abc6693c817ed6f3329b5d843864812b3
Yiyuan7 <31291519+Yiyuan7@users.noreply.github.com>
2018-09-18 01:46:17 -0400

    acc_code

---
+++ acc_code.ino
@@ -0,0 +1,70 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Basic demo for accelerometer readings from Adafruit LIS3DH
+#include <Wire.h>
+
+#include <Adafruit_LIS3DH.h>
+
+// I2C
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+   #define Serial SerialUSB
+#endif
+
+void setup(void) {
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+
+  Serial.begin(9600);
+  Serial.println("LIS3DH test!");
+  
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  
+  Serial.println("LIS3DH found!");
+  
+  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+  
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());  
+  Serial.println("G");
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+}
+
+
+void loop() {
+  lis.read();      // get X Y and Z data at once
+  // Then print out the raw data
+  Serial.print("X:  "); Serial.print(lis.x); 
+  Serial.print("  \tY:  "); Serial.print(lis.y); 
+  Serial.print("  \tZ:  "); Serial.print(lis.z); 
+
+  Serial.println();
+
+  //print X Y and Z data on LCD display
+
+  lcd.setCursor(0, 0);
+  lcd.print(lis.x);
+  lcd.setCursor(4, 0);
+  lcd.print(',');
+  lcd.setCursor(6, 0);
+  lcd.print(lis.y);
+  lcd.setCursor(10, 0);
+  lcd.print(',');
+  lcd.setCursor(12, 0);
+  lcd.print(lis.z);
+ 
+  delay(3000); 
+}
